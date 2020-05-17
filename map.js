0f897f18d23430c67d7dfa17b466d462bddf83eb
Jay686 <camglory83@gmail.com>
2020-05-16 20:36:49 -0700

    api

--- map.js
+++ map.js
@@ -27,7 +27,7 @@ function loadScript()
 {
     var script = document.createElement("script");
     script.type = "text/javascript";
-    script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyDYUY3ksln3b4qH9i1VF978amQ8fy4ZQWc&sensor=false&callback=initialize";
+    script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyDJW4jsPlNKgv6jFm3B5Edp5ywgdqLWdmc&sensor=false&callback=initialize";
     document.body.appendChild(script);
 }
 
